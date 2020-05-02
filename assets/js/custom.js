0c48d2e67086fcb1152733f003f11ca4fabb9593
Lucas Mendelowski <lucas@mendelowski.com>
2020-05-02 23:16:03 +0200

    Add ga

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
   Turbolinks.setProgressBarDelay(100);
 });
 
-document.addEventListener('turbolinks:load', function() {
+document.addEventListener('turbolinks:load', function(event) {
+  if(typeof ga === 'function') {
+    ga("set", "location", event.data.url)
+    ga("send", "pageview")
+  }
+
   initSearch();
 });
